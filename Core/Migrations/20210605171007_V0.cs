7e5b450312b868f09f39faf6f54ad7136c294ca8
Huy Huynh <rinngongan2603@gmail.com>
2021-06-06 00:13:24 +0700

    Migrations

---
+++ Core/Migrations/20210605171007_V0.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Core.Migrations
+{
+    public partial class V0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    UserName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DisplayName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, defaultValueSql: "(N'Admin')"),
+                    PassWord = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false, defaultValueSql: "((0))"),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Account__C9F2845720CEE35B", x => x.UserName);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FoodCategory",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, defaultValueSql: "(N'Chưa đặt tên')")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodCategory", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TableFood",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, defaultValueSql: "(N'Bàn chưa có tên')"),
+                    status = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, defaultValueSql: "(N'Trống')")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TableFood", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Food",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, defaultValueSql: "(N'Chưa đặt tên')"),
+                    idCategory = table.Column<int>(type: "int", nullable: false),
+                    price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Food", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__Food__price__31EC6D26",
+                        column: x => x.idCategory,
+                        principalTable: "FoodCategory",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bill",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateCheckIn = table.Column<DateTime>(type: "date", nullable: false, defaultValueSql: "(getdate())"),
+                    DateCheckOut = table.Column<DateTime>(type: "date", nullable: true),
+                    idTable = table.Column<int>(type: "int", nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false),
+                    discount = table.Column<int>(type: "int", nullable: true),
+                    totalPrice = table.Column<double>(type: "float", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bill", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__Bill__status__36B12243",
+                        column: x => x.idTable,
+                        principalTable: "TableFood",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BillInfo",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    idBill = table.Column<int>(type: "int", nullable: false),
+                    idFood = table.Column<int>(type: "int", nullable: false),
+                    count = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BillInfo", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__BillInfo__count__3A81B327",
+                        column: x => x.idBill,
+                        principalTable: "Bill",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK__BillInfo__idFood__3B75D760",
+                        column: x => x.idFood,
+                        principalTable: "Food",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bill_idTable",
+                table: "Bill",
+                column: "idTable");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BillInfo_idBill",
+                table: "BillInfo",
+                column: "idBill");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BillInfo_idFood",
+                table: "BillInfo",
+                column: "idFood");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Food_idCategory",
+                table: "Food",
+                column: "idCategory");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Account");
+
+            migrationBuilder.DropTable(
+                name: "BillInfo");
+
+            migrationBuilder.DropTable(
+                name: "Bill");
+
+            migrationBuilder.DropTable(
+                name: "Food");
+
+            migrationBuilder.DropTable(
+                name: "TableFood");
+
+            migrationBuilder.DropTable(
+                name: "FoodCategory");
+        }
+    }
+}
---
+++ Core/v0.sql
@@ -0,0 +1,90 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Account] (
+    [UserName] nvarchar(100) NOT NULL,
+    [DisplayName] nvarchar(100) NOT NULL DEFAULT ((N'Admin')),
+    [PassWord] nvarchar(1000) NOT NULL DEFAULT (((0))),
+    [Type] int NOT NULL,
+    CONSTRAINT [PK__Account__C9F2845720CEE35B] PRIMARY KEY ([UserName])
+);
+GO
+
+CREATE TABLE [FoodCategory] (
+    [id] int NOT NULL IDENTITY,
+    [name] nvarchar(100) NOT NULL DEFAULT ((N'Chưa đặt tên')),
+    CONSTRAINT [PK_FoodCategory] PRIMARY KEY ([id])
+);
+GO
+
+CREATE TABLE [TableFood] (
+    [id] int NOT NULL IDENTITY,
+    [name] nvarchar(100) NOT NULL DEFAULT ((N'Bàn chưa có tên')),
+    [status] nvarchar(100) NOT NULL DEFAULT ((N'Trống')),
+    CONSTRAINT [PK_TableFood] PRIMARY KEY ([id])
+);
+GO
+
+CREATE TABLE [Food] (
+    [id] int NOT NULL IDENTITY,
+    [name] nvarchar(100) NOT NULL DEFAULT ((N'Chưa đặt tên')),
+    [idCategory] int NOT NULL,
+    [price] float NOT NULL,
+    CONSTRAINT [PK_Food] PRIMARY KEY ([id]),
+    CONSTRAINT [FK__Food__price__31EC6D26] FOREIGN KEY ([idCategory]) REFERENCES [FoodCategory] ([id]) ON DELETE NO ACTION
+);
+GO
+
+CREATE TABLE [Bill] (
+    [id] int NOT NULL IDENTITY,
+    [DateCheckIn] date NOT NULL DEFAULT ((getdate())),
+    [DateCheckOut] date NULL,
+    [idTable] int NOT NULL,
+    [status] int NOT NULL,
+    [discount] int NULL,
+    [totalPrice] float NULL,
+    CONSTRAINT [PK_Bill] PRIMARY KEY ([id]),
+    CONSTRAINT [FK__Bill__status__36B12243] FOREIGN KEY ([idTable]) REFERENCES [TableFood] ([id]) ON DELETE NO ACTION
+);
+GO
+
+CREATE TABLE [BillInfo] (
+    [id] int NOT NULL IDENTITY,
+    [idBill] int NOT NULL,
+    [idFood] int NOT NULL,
+    [count] int NOT NULL,
+    CONSTRAINT [PK_BillInfo] PRIMARY KEY ([id]),
+    CONSTRAINT [FK__BillInfo__count__3A81B327] FOREIGN KEY ([idBill]) REFERENCES [Bill] ([id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK__BillInfo__idFood__3B75D760] FOREIGN KEY ([idFood]) REFERENCES [Food] ([id]) ON DELETE NO ACTION
+);
+GO
+
+CREATE INDEX [IX_Bill_idTable] ON [Bill] ([idTable]);
+GO
+
+CREATE INDEX [IX_BillInfo_idBill] ON [BillInfo] ([idBill]);
+GO
+
+CREATE INDEX [IX_BillInfo_idFood] ON [BillInfo] ([idFood]);
+GO
+
+CREATE INDEX [IX_Food_idCategory] ON [Food] ([idCategory]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210605171007_V0', N'5.0.6');
+GO
+
+COMMIT;
+GO
+
